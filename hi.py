662bfc1e097aacac63b9ddbed9399d65f8cfa3aa
swaroopedupulapati <swaroopedupulapati1@gmail.com>
2025-01-18 22:18:37 +0530

    exp5

--- hi.py
+++ hi.py
@@ -1,25 +1,57 @@
-import numpy  as np 
+"""import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
+import seaborn as sns
 from sklearn.model_selection import train_test_split 
 from sklearn.linear_model import LinearRegression 
 from sklearn.metrics import r2_score
 #Loadingdataset
-dataset=pd.read_csv('Salary_Data.csv') 
-#Feature Extraction
-X=dataset.iloc[:,:-1].values 
-y = dataset.iloc[:,-1].values 
-#splitting Train & Test data
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.3) 
-#Linear Rgression Model Creation
-reg_model = LinearRegression() 
-#ModelTraining(Fitthemodel) 
-reg_model.fit(X_train,y_train) 
-#Model Prediction
+house_df=pd.read_csv('housing.csv') 
+print("Housing dataset columns:") 
+print(house_df.columns)
+#FeatureExtraction
+X=house_df[['Avg.Area Income','Avg.Area House Age','Avg.Area Number of Rooms','Avg.Area Number of Bedrooms','Area Population']] 
+y =house_df['Price']
+#SplittingTrainandTestdata
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size= 0.3)
+#LinearRegresionModelCreation
+reg_model =LinearRegression()
+#Traing the model (fit)
+reg_model.fit(X_train,y_train)
+#ModelPrediction
 y_pred=reg_model.predict(X_test)
- #Finding R-Sqaure value
-print("R-Sqaure value(accuracy):",r2_score(y_test,y_pred)) 
-#Visualizing the graph
-plt.scatter(X_test,y_test, color='red') 
-plt.plot(X_test, y_pred,color='blue')
+#Accuracyofmodel
+print("R-Sqaurevalue",r2_score(y_test,y_pred))
+#Visualization
+plt.scatter(y_test,y_pred) 
+plt.show()"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt 
+import seaborn as sns
+from sklearn.model_selection import train_test_split 
+from sklearn.linear_model import LinearRegression 
+from sklearn.metrics import r2_score
+#Loadingdataset
+house_df=pd.read_csv('housing.csv') 
+print("Housing dataset columns:") 
+print(house_df.columns)
+#FeatureExtraction
+X=house_df[['Avg. Area Income','Avg. Area House Age','Avg. Area Number of Rooms',
+            'Avg. Area Number of Bedrooms','Area Population']] 
+
+y =house_df['Price']
+#SplittingTrainandTestdata
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size= 0.3)
+#LinearRegresionModelCreation
+reg_model =LinearRegression()
+#Traing the model (fit)
+reg_model.fit(X_train,y_train) 
+#ModelPrediction
+y_pred=reg_model.predict(X_test)
+#Accuracyofmodel
+print("R-Sqaurevalue",r2_score(y_test,y_pred))
+#Visualization
+plt.scatter(y_test,y_pred) 
 plt.show()
